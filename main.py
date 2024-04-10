7aa7e67bef5adbed70874f23870fa0efedf261cf
YiPei1994 <studentyp@gmail.com>
2024-04-10 21:04:58 +0200

    a description of your changes here

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    lower = get_number_of_letter(text)
-    print(lower)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+
+    listed = []
+
+    
+    for chars in chars_dict:
+        if chars.isalpha():
+            new_dict = {}
+            new_dict['name'] = chars
+            new_dict['num'] = chars_dict[chars]
+            listed.append(new_dict)
+
+    sorted_list = sorted(listed, key=lambda k: k.get('num'),reverse=True)
+
+    print('---  Begin report of books/frankenstein.txt ---')
+    print(f'{num_words} words found in the document')
+    print('')
+    for dict in sorted_list:
+     
+        print(f'The "{dict["name"]}" character was found {dict["num"]} times')     
+
+    print('--- End report ---')
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_number_of_letter(text):
-    lower = text.lower()
-    char = [*lower]
-    count = {}
-    for c in char:
-        count[char[c]] = 1
-        if char[c] in count:
-            count[char[c]] += 1
-                
-    return char
+def sort_on(dict):
+    return dict['num']
 
 main()
